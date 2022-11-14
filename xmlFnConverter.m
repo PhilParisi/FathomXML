09f6ac414ef745d6b968cd952daec8112b112c28
Jake Bonney <jakeboney10@gmail.com>
2022-11-14 11:33:56 -0500

    Added script to parse through xml and convert to fathom net format
    Updated format to xls in xmlToCsvAppend

---
+++ xmlFnConverter.m
@@ -0,0 +1,46 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% xmlFnConverter(xml_filename,csv_filename)
+%
+% Developer : Jbuns
+%
+% For Usage in Formatting Data to Send to Fathomnet.
+%
+% This function takes an xml annotation file and a CSV filename as inputs. 
+% The output appends a struct to a CSV file with the annotation information 
+% in Fathomnet format.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function xmlParser(xml_filename,csv_filename)
+
+y = xml2struct(xml_filename); % create nested struct from XML file
+
+if isfield(y.annotation, 'object') % check for field 'object' in struct
+    disp('Houston, we have an urchin!')
+    
+    loc = findLocation(xml_filename); % find Hawaii location from filename
+    
+    for i = 1:length(y.annotation.object) % iterate for each object in xml
+        
+        % parse annotation data from struct
+        name = y.annotation.object{1,i}.name.Text;
+        xmin = str2num(y.annotation.object{i}.bndbox.xmin.Text);
+        ymin = str2num(y.annotation.object{i}.bndbox.ymin.Text);
+        xmax = str2num(y.annotation.object{i}.bndbox.xmax.Text);
+        ymax = str2num(y.annotation.object{i}.bndbox.ymax.Text);
+        
+        % create struct of class object
+        object.class = name;
+        object.url = ('url placeholder');
+        object.x = xmin;
+        object.y = ymin;
+        object.width = xmax - xmin;
+        object.height = ymax - ymin;
+        object.location = loc;
+        
+        xmlToCsvAppend(object, csv_filename); % aopend object to CSV file
+    end
+    
+else 
+    disp('Sorry, no urchins :(')
+end
--- xmlToCsvAppend.m
+++ xmlToCsvAppend.m
@@ -1,9 +1,5 @@
 function xmlToCsvAppend(data_struct,filename)
 
-% convert to table
 table_data = struct2table(data_struct);
 
-% write the table to a .csv file
-writetable(table_data,filename,'WriteMode','Append');
-
-end
+writetable(table_data,filename,'WriteMode','Append','FileType', 'spreadsheet');
