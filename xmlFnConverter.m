1084b7c2e034b0d5e40f4353a0375d53e3fc11e2
jake <jakebonney10@gmail.com>
2022-11-17 13:13:42 -0500

    Updated xmlFnConverter and added image folder.

--- xmlFnConverter.m
+++ xmlFnConverter.m
@@ -20,19 +20,30 @@ if isfield(y.annotation, 'object') % check for field 'object' in struct
     
     loc = findLocation(xml_filename); % find Hawaii location from filename
     
+    [FILEPATH,NAME,EXT] = fileparts(xml_filename); % change ext to .jpg
+    jpg_filename = append(NAME, ".jpg")
+    
     for i = 1:length(y.annotation.object) % iterate for each object in xml
         
-        % parse annotation data from struct
-        name = y.annotation.object{1,i}.name.Text;
-        xmin = str2num(y.annotation.object{i}.bndbox.xmin.Text);
-        ymin = str2num(y.annotation.object{i}.bndbox.ymin.Text);
-        xmax = str2num(y.annotation.object{i}.bndbox.xmax.Text);
-        ymax = str2num(y.annotation.object{i}.bndbox.ymax.Text);
+        % parse annotation data from struct, check if greater than 1 object
+        % because struct indexing changes (i hate matlab).
+        if length(y.annotation.object) <= 1
+            name = y.annotation.object.name.Text;
+            xmin = str2num(y.annotation.object.bndbox.xmin.Text);
+            ymin = str2num(y.annotation.object.bndbox.ymin.Text);
+            xmax = str2num(y.annotation.object.bndbox.xmax.Text);
+            ymax = str2num(y.annotation.object.bndbox.ymax.Text);
+        else
+            name = y.annotation.object{1,i}.name.Text;
+            xmin = str2num(y.annotation.object{i}.bndbox.xmin.Text);
+            ymin = str2num(y.annotation.object{i}.bndbox.ymin.Text);
+            xmax = str2num(y.annotation.object{i}.bndbox.xmax.Text);
+            ymax = str2num(y.annotation.object{i}.bndbox.ymax.Text);
+        end
         
         % create struct of class object
-        object.filename = xml_filename;
+        object.filename = jpg_filename;
         object.class = name;
-        object.url = ('url_placeholder');
         object.x = xmin;
         object.y = ymin;
         object.width = xmax - xmin;
